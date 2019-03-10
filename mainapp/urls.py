4d5ed55ab20f453be355c0f1b90fa9e793df232c
Innokenty Malinnikov <ken.malinnikov@gmail.com>
2019-03-10 20:58:04 +0200

    Added pages Login, Sign Up.

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns= [
     path('', views.index, name = 'index'),
     path('titan', views.titan, name = 'titan'),
+    path('login', views.login, name = 'login'),
+    path('signup', views.signUp, name = 'signUp'),
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,10 +1,50 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+
+MAIN_PAGE_TITLE = "Main page"
+TITAN_COLOSS_TITLE = "Armored Titan & Titan Coloss"
 
 
 # Create your views here.
 def index(request):
-    return render(request, "wrapper.html")
+    contentTamplate = loader.get_template('main_page_content.html')
+    context = {
+        'title': MAIN_PAGE_TITLE,
+        'linkAddress': "titan",
+        'linkName': TITAN_COLOSS_TITLE,
+        'content': contentTamplate.render(),
+    }
+    return createPage(context, request)
 
 
 def titan(request):
-    return render(request, "titan.html")
+    contentTamplate = loader.get_template('titan.html')
+    context = {
+        'title': TITAN_COLOSS_TITLE,
+        'linkAddress': "/",
+        'linkName': MAIN_PAGE_TITLE,
+        'content': contentTamplate.render(),
+    }
+    return createPage(context, request)
+
+
+def login(request):
+    return signUp(request, "none")
+
+
+def signUp(request, display=''):
+    contentContext = {'display': display}
+    contentTamplate = loader.get_template('auth.html')
+    context = {
+        'title': TITAN_COLOSS_TITLE,
+        'linkAddress': "/",
+        'linkName': MAIN_PAGE_TITLE,
+        'content': contentTamplate.render(contentContext),
+    }
+    return createPage(context, request)
+
+
+def createPage(context, request):
+    template = loader.get_template('wrapper.html')
+    return HttpResponse(template.render(context, request))
